8842ae9f46e16ba08b7e8537c1f4b200250d3e6d
Marijn van Zelst <marijnvanzelst@gmail.com>
2019-02-17 15:04:20 +0100

    Added item type field, added conjured item logic

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,51 +1,68 @@
 package com.gildedrose;
 
+import java.util.Objects;
+
+import static com.gildedrose.Item.ItemType.LEGENDARY;
+import static com.gildedrose.Item.ItemType.NORMAL;
+
 public class Item {
 
-    private String name;
+    private final String name;
 
     private int sellIn;
 
     private int quality;
 
-    public Item(String name, int sellIn, int quality) {
+    private final ItemType itemType;
+
+    public Item(String name, int sellIn, int quality, ItemType itemType) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        this.itemType = Objects.requireNonNull(itemType);
         assertQuality();
     }
 
     public void decreaseSellInAndUpdateQuality() {
         sellIn--;
 
-        if ("Sulfuras, Hand of Ragnaros".equals(name)) {
-            return;
+        switch (itemType){
+            case LEGENDARY:
+                break;
+            case BACKSTAGE_PASSES:
+                updateBackstagePassesQuality();
+                break;
+            case AGED_BRIE:
+                incrementQualityBetweenZeroAndFifty(1);
+                break;
+            case NORMAL:
+            case CONJURED:
+                updateNormalAndConjuredQuality();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown Item Type");
         }
+    }
 
-        if ("Backstage passes to a TAFKAL80ETC concert".equals(name)) {
-            if (sellIn < 0) {
-                setQuality(0);
+    private void updateBackstagePassesQuality() {
+        if (sellIn < 0) {
+            setQuality(0);
+        } else {
+            if (sellIn < 5) {
+                incrementQualityBetweenZeroAndFifty(3);
+            } else if (sellIn < 10) {
+                incrementQualityBetweenZeroAndFifty(2);
             } else {
-                if (sellIn < 5) {
-                    incrementQualityBetweenZeroAndFifty(3);
-                } else if (sellIn < 10) {
-                    incrementQualityBetweenZeroAndFifty(2);
-                } else {
-                    incrementQualityBetweenZeroAndFifty(1);
-                }
+                incrementQualityBetweenZeroAndFifty(1);
             }
-            return;
         }
+    }
 
-        if ("Aged Brie".equals(name)) {
-            incrementQualityBetweenZeroAndFifty(1);
-            return;
-        }
-
+    private void updateNormalAndConjuredQuality() {
         if (quality > 0 && sellIn < 0) {
-            incrementQualityBetweenZeroAndFifty(-2);
+            incrementQualityBetweenZeroAndFifty(itemType == NORMAL ? -2 : -4);
         } else if (quality > 0) {
-            incrementQualityBetweenZeroAndFifty(-1);
+            incrementQualityBetweenZeroAndFifty(itemType == NORMAL ? -1 : -2);
         }
     }
 
@@ -84,9 +101,9 @@ public class Item {
             throw new IllegalArgumentException("Quality cannot be negative");
         }
 
-        if ("Sulfuras, Hand of Ragnaros".equals(name)) {
+        if (itemType == LEGENDARY) {
             if (quality != 80) {
-                throw new IllegalArgumentException("Quality of Sulfuras, Hand of Ragnaros must be 80");
+                throw new IllegalArgumentException("Quality of Legendary items must be 80");
             } else {
                 return;
             }
@@ -99,6 +116,14 @@ public class Item {
 
     @Override
     public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+        return this.name + ", " + this.sellIn + ", " + this.quality + ", " + this.itemType;
+    }
+
+    public enum ItemType {
+        NORMAL,
+        AGED_BRIE,
+        BACKSTAGE_PASSES,
+        LEGENDARY,
+        CONJURED
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,28 +2,31 @@ package com.gildedrose;
 
 import org.junit.Test;
 
+import static com.gildedrose.Item.ItemType.*;
 import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void qualityOfItemCannotBeNegative() {
-        new Item("foo", 0, -1);
+        new Item("foo", 0, -1, NORMAL);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void sulfurasMustBe80() {
-        new Item("Sulfuras, Hand of Ragnaros", 0, 70);
+    public void legendaryQualityMustBeEighty() {
+        new Item("Sulfuras, Hand of Ragnaros", 0, 70, LEGENDARY);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void qualityCannotBeHigherThan50() {
-        new Item("foo", 0, 51);
+    public void qualityCannotBeHigherThanFifty() {
+        new Item("foo", 0, 51, NORMAL);
     }
 
     @Test
     public void qualityOfItemCannotBeNegativeOnUpdate() {
-        Item[] items = new Item[] { new Item("foo", 0, 1), new Item("foo", 1, 0)};
+        Item[] items = new Item[] {
+                new Item("foo", 0, 1, NORMAL),
+                new Item("foo", 1, 0, NORMAL)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(0, app.items[0].getQuality());
@@ -32,15 +35,31 @@ public class GildedRoseTest {
 
     @Test
     public void qualityDegradesByOneWhenSellByDateNotPassed() {
-        Item[] items = new Item[] { new Item("foo", 1, 2) };
+        Item[] items = new Item[] { new Item("foo", 1, 2, NORMAL) };
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(1, app.items[0].getQuality());
     }
 
     @Test
-    public void qualityDegradesTwiceAsFastWhenPastSellByDate() {
-        Item[] items = new Item[] { new Item("foo", 0, 3), new Item("foo", -1, 3) };
+    public void qualityDegradesByTwoWhenSellByDateNotPassedAndItemIsConjured() {
+        Item[] items = new Item[] {
+                new Item("Conjured foo", 1, 1, CONJURED),
+                new Item("Conjured foo", 1, 2, CONJURED),
+                new Item("Conjured foo", 1, 3, CONJURED)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateInventoryQuality();
+        assertEquals(0, app.items[0].getQuality());
+        assertEquals(0, app.items[1].getQuality());
+        assertEquals(1, app.items[2].getQuality());
+    }
+
+    @Test
+    public void qualityDegradesByTwoWhenPastSellByDate() {
+        Item[] items = new Item[] {
+                new Item("foo", 0, 3, NORMAL),
+                new Item("foo", -1, 3, NORMAL) };
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(1, app.items[0].getQuality());
@@ -48,18 +67,34 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void agedBrieIncreasesInQualityTheOlderItGets() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 1),  new Item("Aged Brie", 1, 1)};
+    public void qualityDegradesByFourWhenSellByPassedAndItemIsConjured() {
+        Item[] items = new Item[] {
+                new Item("Conjured foo", 0, 5, CONJURED),
+                new Item("Conjured foo", 0, 3, CONJURED),
+                new Item("Conjured foo", 0, 0, CONJURED)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
-        // TODO: Should the value increase by two on sellDate? Bug or feature?
+        assertEquals(1, app.items[0].getQuality());
+        assertEquals(0, app.items[1].getQuality());
+        assertEquals(0, app.items[2].getQuality());
+    }
+
+    @Test
+    public void agedBrieIncreasesInQualityTheOlderItGets() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 0, 1, AGED_BRIE),
+                new Item("Aged Brie", 1, 1, AGED_BRIE)};
+        GildedRose app = new GildedRose(items);
+        app.updateInventoryQuality();
         assertEquals(2, app.items[0].getQuality());
         assertEquals(2, app.items[1].getQuality());
     }
 
     @Test
     public void qualityOfItemCannotBeHigherThan50() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 50), new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49)};
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 0, 50, AGED_BRIE),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49, BACKSTAGE_PASSES)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(50, app.items[0].getQuality());
@@ -67,10 +102,11 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void sulfurasNeverDegradesInQuality() {
-        Item[] items = new Item[]{ new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-                new Item("Sulfuras, Hand of Ragnaros", 1, 80),
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80)};
+    public void legendaryItemsNeverDegradesInQuality() {
+        Item[] items = new Item[]{
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80, LEGENDARY),
+                new Item("Sulfuras, Hand of Ragnaros", 1, 80, LEGENDARY),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80, LEGENDARY)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(80, app.items[0].getQuality());
@@ -81,8 +117,8 @@ public class GildedRoseTest {
     @Test
     public void backStagePassesIncreaseInQualityBy2WhenBetween6And10DayLeft() {
         Item[] items = new Item[]{
-                new Item("Backstage passes to a TAFKAL80ETC concert", 6, 2),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 2)};
+                new Item("Backstage passes to a TAFKAL80ETC concert", 6, 2, BACKSTAGE_PASSES),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 2, BACKSTAGE_PASSES)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(4, app.items[0].getQuality());
@@ -92,8 +128,8 @@ public class GildedRoseTest {
     @Test
     public void backStagePassesIncreaseInQualityBy3WhenLessThan5DaysLeft() {
         Item[] items = new Item[]{
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 2),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 2)};
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 2, BACKSTAGE_PASSES),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 2, BACKSTAGE_PASSES)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(5, app.items[0].getQuality());
@@ -103,8 +139,8 @@ public class GildedRoseTest {
     @Test
     public void backStagePassesDropToZeroWhenConcertPassed() {
         Item[] items = new Item[]{
-                new Item("Backstage passes to a TAFKAL80ETC concert", -1, 2),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 2)};
+                new Item("Backstage passes to a TAFKAL80ETC concert", -1, 2, BACKSTAGE_PASSES),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 2, BACKSTAGE_PASSES)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(0, app.items[0].getQuality());
@@ -114,8 +150,8 @@ public class GildedRoseTest {
     @Test
     public void backStagePassesDecreaseBy1InQualityWhenMoreThan10DaysLeft() {
         Item[] items = new Item[]{
-                new Item("Backstage passes to a TAFKAL80ETC concert", 11, 2),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 50, 2)};
+                new Item("Backstage passes to a TAFKAL80ETC concert", 11, 2, BACKSTAGE_PASSES),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 50, 2, BACKSTAGE_PASSES)};
         GildedRose app = new GildedRose(items);
         app.updateInventoryQuality();
         assertEquals(3, app.items[0].getQuality());
